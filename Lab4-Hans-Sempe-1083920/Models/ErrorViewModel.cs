e0d36612c4ad93929f75e46311412e59e90375bd
Pierriny <70544564+Pierriny@users.noreply.github.com>
2022-03-26 19:29:54 -0600

    Agregar archivos de proyecto.

---
+++ Lab4-Hans-Sempe-1083920/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab4_Hans_Sempe_1083920.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
